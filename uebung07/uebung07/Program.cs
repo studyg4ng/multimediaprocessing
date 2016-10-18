926880521b809b72c7f538b05edf8eac571d5ad9
Thomas Peter Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2016-10-18 12:50:51 +0200

    start with uebung07

---
+++ uebung07/uebung07/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uebung07 {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
