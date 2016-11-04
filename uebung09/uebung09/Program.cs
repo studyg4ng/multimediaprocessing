abd209dd5a9f8893830bdcabf01d1b61b5d7b758
pat <pitjthjrthiojth@gmail.com>
2016-11-04 09:05:44 +0100

    created projects, added headers

---
+++ uebung09/uebung09/Program.cs
@@ -0,0 +1,18 @@
+﻿/*1510601027 fhs38532
+  Thomas Siller
+  -------------------
+  1510601032 fhs38596
+  Patrick Obermüller*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uebung09 {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
