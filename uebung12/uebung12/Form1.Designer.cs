30a5c0cc89e4fcd7293b29962a10425f265ad481
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2016-11-05 17:41:02 +0100

    start uebung12

---
+++ uebung12/uebung12/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace uebung12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbOriginal = new System.Windows.Forms.PictureBox();
+            this.pbFilter = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnImageSelect = new System.Windows.Forms.Button();
+            this.txtImagePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.gbFilters = new System.Windows.Forms.GroupBox();
+            this.rbClamp = new System.Windows.Forms.RadioButton();
+            this.rbThreshold = new System.Windows.Forms.RadioButton();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFilter)).BeginInit();
+            this.gbFilters.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbOriginal
+            // 
+            this.pbOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbOriginal.Location = new System.Drawing.Point(12, 239);
+            this.pbOriginal.Name = "pbOriginal";
+            this.pbOriginal.Size = new System.Drawing.Size(300, 300);
+            this.pbOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbOriginal.TabIndex = 0;
+            this.pbOriginal.TabStop = false;
+            // 
+            // pbFilter
+            // 
+            this.pbFilter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbFilter.Location = new System.Drawing.Point(318, 239);
+            this.pbFilter.Name = "pbFilter";
+            this.pbFilter.Size = new System.Drawing.Size(300, 300);
+            this.pbFilter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbFilter.TabIndex = 1;
+            this.pbFilter.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Original:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Filter:";
+            // 
+            // btnImageSelect
+            // 
+            this.btnImageSelect.Location = new System.Drawing.Point(461, 12);
+            this.btnImageSelect.Name = "btnImageSelect";
+            this.btnImageSelect.Size = new System.Drawing.Size(157, 23);
+            this.btnImageSelect.TabIndex = 4;
+            this.btnImageSelect.Text = "Bild auswählen";
+            this.btnImageSelect.UseVisualStyleBackColor = true;
+            this.btnImageSelect.Click += new System.EventHandler(this.btnImageSelect_Click);
+            // 
+            // txtImagePath
+            // 
+            this.txtImagePath.Location = new System.Drawing.Point(12, 14);
+            this.txtImagePath.Name = "txtImagePath";
+            this.txtImagePath.Size = new System.Drawing.Size(443, 20);
+            this.txtImagePath.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // gbFilters
+            // 
+            this.gbFilters.Controls.Add(this.rbThreshold);
+            this.gbFilters.Controls.Add(this.rbClamp);
+            this.gbFilters.Location = new System.Drawing.Point(12, 40);
+            this.gbFilters.Name = "gbFilters";
+            this.gbFilters.Size = new System.Drawing.Size(606, 164);
+            this.gbFilters.TabIndex = 6;
+            this.gbFilters.TabStop = false;
+            this.gbFilters.Text = "Filter";
+            // 
+            // rbClamp
+            // 
+            this.rbClamp.AutoSize = true;
+            this.rbClamp.Location = new System.Drawing.Point(6, 19);
+            this.rbClamp.Name = "rbClamp";
+            this.rbClamp.Size = new System.Drawing.Size(53, 17);
+            this.rbClamp.TabIndex = 0;
+            this.rbClamp.TabStop = true;
+            this.rbClamp.Text = "clamp";
+            this.rbClamp.UseVisualStyleBackColor = true;
+            this.rbClamp.CheckedChanged += new System.EventHandler(this.rbClamp_CheckedChanged);
+            // 
+            // rbThreshold
+            // 
+            this.rbThreshold.AutoSize = true;
+            this.rbThreshold.Location = new System.Drawing.Point(6, 42);
+            this.rbThreshold.Name = "rbThreshold";
+            this.rbThreshold.Size = new System.Drawing.Size(68, 17);
+            this.rbThreshold.TabIndex = 1;
+            this.rbThreshold.TabStop = true;
+            this.rbThreshold.Text = "threshold";
+            this.rbThreshold.UseVisualStyleBackColor = true;
+            this.rbThreshold.CheckedChanged += new System.EventHandler(this.rbThreshold_CheckedChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 210);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(606, 10);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 551);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.gbFilters);
+            this.Controls.Add(this.txtImagePath);
+            this.Controls.Add(this.btnImageSelect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbFilter);
+            this.Controls.Add(this.pbOriginal);
+            this.Name = "Form1";
+            this.Text = "ImageMagic";
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFilter)).EndInit();
+            this.gbFilters.ResumeLayout(false);
+            this.gbFilters.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbOriginal;
+        private System.Windows.Forms.PictureBox pbFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnImageSelect;
+        private System.Windows.Forms.TextBox txtImagePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox gbFilters;
+        private System.Windows.Forms.RadioButton rbThreshold;
+        private System.Windows.Forms.RadioButton rbClamp;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ uebung12/uebung12/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uebung12
+{
+    public partial class Form1 : Form
+    {
+        private string _filePath;
+        private PleaseWaitForm _pleaseWait;
+        private bool _wait = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pleaseWait()
+        {
+            if(_pleaseWait == null) _pleaseWait = new PleaseWaitForm();
+            if (_wait)
+            {
+                _wait = false;
+                _pleaseWait.Hide();
+            }
+            else {
+                _wait = true;
+                _pleaseWait.Show();
+            }
+            
+        }
+
+        private void updateFilterImage(Bitmap filteredImage)
+        {
+            pbFilter.Image = filteredImage;
+            pbFilter.Update();
+        }
+
+        private void btnImageSelect_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                _filePath = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(_filePath);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error");
+                }
+                txtImagePath.Text = _filePath;
+                pbOriginal.ImageLocation = _filePath;
+                pbOriginal.Update();
+            }
+        }
+
+        private void rbClamp_CheckedChanged(object sender, EventArgs e)
+        {
+            pleaseWait();
+            updateFilterImage(new Clamp(_filePath, 100, 150).invertImage());
+            pleaseWait();
+        }
+
+        private void rbThreshold_CheckedChanged(object sender, EventArgs e)
+        {
+            pleaseWait();
+            updateFilterImage(new Threshold(_filePath, 128).invertImage());
+            pleaseWait();
+        }
+    }
+}
