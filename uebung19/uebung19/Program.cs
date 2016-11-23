a3d4b5658cadb7527307991c24021357d67758b2
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2016-11-23 18:54:50 +0100

    finished uebung19

--- uebung19/uebung19/Program.cs
+++ uebung19/uebung19/Program.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,22 +17,66 @@ namespace uebung19
     {
         static void Main(string[] args)
         {
-            byte[,] image = new byte[11, 11] {
-                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            byte[,] erodeMatrix = new byte[3, 3] {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
             };
 
-            byte[,] image2 = new byte[11, 11] {
-                { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            Erode ero = new Erode(erodeMatrix);
+
+            byte[,] erodeCrossMatrix = new byte[3, 3] {
+                { 0, 1, 0 },
+                { 1, 1, 1 },
+                { 0, 1, 0 }
+            };
+
+            Erode eroCross = new Erode(erodeCrossMatrix);
+
+            byte[,] imageA = new byte[11, 11] {
+                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0 }
+            };
+
+            byte[,] imageASolution = new byte[11, 11] {
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }
+            };
+
+            byte[,] imageACrossSolution = new byte[11, 11] {
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }
+            };
+
+            byte[,] imageB = new byte[11, 11] {
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
@@ -44,16 +89,33 @@ namespace uebung19
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
             };
 
-            byte[,] dilateFilter = new byte[3, 3] {
-                { 1, 1, 1 },
-                { 1, 1, 1 },
-                { 1, 1, 1 }
-            };
+            byte[,] erodedImageA = ero.execute(imageA);
+            compareImages(imageASolution, erodedImageA);
 
-            Erode ero = new Erode(dilateFilter);
-            byte[,] tmp = ero.execute(image2);
-            byte[,] tmp2 = ero.execute(tmp);
+            byte[,] erodedImageB = ero.execute(imageB);
+            compareImages(imageB, erodedImageB);
+
+            byte[,] erodedCrossImageA = eroCross.execute(imageA);
+            compareImages(imageACrossSolution, erodedCrossImageA);
+
+            byte[,] erodedCrossImageB = eroCross.execute(imageB);
+            compareImages(imageB, erodedCrossImageB);
+
             Console.WriteLine("");
+        }
+
+        private static void compareImages(byte[,] imageA, byte[,] imageB)
+        {
+            int width = imageA.GetLength(0);
+            int height = imageA.GetLength(0);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Debug.Assert(imageA[x, y].Equals(imageB[x, y]));
+                }
+            }
         }
     }
 }
